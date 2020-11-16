b51acae08d75e52111bd87492c809753377bd5c7
pDallastra <52924113+pDallastra@users.noreply.github.com>
2020-11-16 16:06:54 -0300

    Forms working, missing styling

---
+++ src/components/Form.vue
@@ -0,0 +1,84 @@
+<template>
+<form @submit.prevent="FormHandler">
+    <input type="text" placeholder="Type" v-model="formData.desc">
+    <input type="number" placeholder="Amount" v-model="formData.value">
+    <input type="date" placeholder="Date" v-model="formData.date">
+    <input type="submit" placeholder="Submit">
+</form>
+</template>
+
+<script>
+import {
+    reactive
+} from 'vue'
+export default {
+    props: {
+        state: Object
+    },
+    setup(props, {
+        emit
+    }) {
+        const formData = reactive({
+            desc: null,
+            value: null,
+            date: null
+        });
+
+        function FormHandler() {
+            emit("add-income", {
+                desc: formData.desc,
+                value: formData.value,
+                date: formData.date
+            });
+
+            formData.desc = null,
+                formData.value = null,
+                formData.date = null
+        }
+
+        return {
+            FormHandler,
+            formData
+        }
+    }
+}
+</script>
+
+<style scoped>
+/* form {
+    display: flex;
+    justify-content: center;
+    margin-top: 30px;
+}
+
+form input {
+    display: block;
+    border: none;
+    outline: none;
+    padding: 5px 15px;
+    background-color: #fff;
+    color: #888;
+    font-size: 20px;
+}
+
+form input::placeholder {
+    color: #AAA;
+}
+
+form input[type="submit"] {
+    background: none;
+    color: #fff;
+    font-weight: 500;
+    text-shadow: 0px 1px 3px #00000033;
+    background-color: #ffce00;
+    cursor: pointer;
+}
+
+form input:first-of-type {
+    border-radius: 8px 0px 0px 8px;
+}
+
+form input:last-of-type {
+    border-radius: 0px 8px 8px 0px;
+} */
+</style>
---
+++ src/components/IncomeItem.vue
@@ -0,0 +1,67 @@
+<template>
+<div class="incomeItem">
+    <div class="removeItem">x</div>
+    <div class="desc">{{income.desc}}</div>
+    <div class="price">U${{income.value}}</div>
+    <div class="date">{{displayDate}}</div>
+</div>
+</template>
+
+<script>
+export default {
+    props: {
+        income: Object
+    },
+    setup(props) {
+        let date = new Date(props.income.date);
+        let day = date.getDate();
+        let month = date.getMonth();
+        let year = date.getFullYear();
+
+        let displayDate = month + "/" + day + "/" + year;
+
+        return {
+            displayDate
+        }
+    }
+}
+</script>
+
+<style>
+/* .incomeItem {
+    position: relative;
+    display: flex;
+    padding: 15px 15px 15px 0;
+    background-color: #fff;
+    border-radius: 8px;
+    max-width: 600px;
+    margin: 0 auto 30px;
+}
+
+.removeItem {
+    color: #EF2D2D;
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 1;
+    text-align: center;
+    margin: 0 15px;
+}
+
+.desc {
+    color: #666;
+    flex: 1 1 100%;
+    font-size: 20px;
+}
+
+.price {
+    color: #666;
+    min-width: 100px;
+    font-size: 20px;
+}
+
+.date {
+    color: #666;
+    text-align: right;
+    font-size: 20px;
+} */
+</style>
